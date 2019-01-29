46a083f7ad4dbd886d202c4c25237a0d10f69119
NisseOscar <o.n.johansson@gmail.com>
2019-01-29 12:02:30 +0100

    DynamicSystemSimulater

---
+++ DynamicSystemSimulater.py
@@ -0,0 +1,7 @@
+import numpy as np
+from math import sin
+import math
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from mpl_toolkits.mplot3d import Axes3D
+from scipy import signal
