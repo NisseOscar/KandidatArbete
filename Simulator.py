e80fba884634baaaeb23ee3c30db2a934bc6fefc
adoJonsson <46928572+adoJonsson@users.noreply.github.com>
2019-02-22 09:49:22 +0100

    Solving merge problem

--- Simulator.py
+++ Simulator.py
@@ -15,8 +15,8 @@ class Simulator:
 
     def states(self,duration,split):
         self.t = np.arange(0.0, duration, split)
-        self.states = odeint(self.function.f, self.state0, self.t) # Choose integrator
-        #self.states = solve_ivp(self.function,[0, duration],self.state0,method='RK45',t_eval=self.t) # RK45, 4th order, 5th order error estimation
+        #self.states = odeint(self.function.f, self.state0, self.t) # Choose integrator
+        self.states = solve_ivp(self.function,[0, duration],self.state0,method='RK45',t_eval=self.t) # RK45, 4th order, 5th order error estimation
         return self.states
 
     def tredimplot(self,dim = [0,1,2]):
@@ -40,16 +40,6 @@ class Simulator:
         states = self.states
         plt.plot(self.t,states[:,dim[0]],alpha = 0.3)
         plt.show()
-<<<<<<< HEAD
-    def saveData(self):
-        if self.states is None:
-            self.states()
-        np.savetxt("data.txt",self.states)
-eq = Equation.Rossler()
-sim = Simulator(function = eq,init = [1.0,8,1.0])
-state = sim.states(600,0.01)
-sim.saveData()
-=======
     def storeData(self,filename):
         np.savetxt(filename+'Values.txt',self.states)
         np.savetxt(filename+'Time.txt',self.t)
@@ -61,4 +51,3 @@ sim.saveData()
 #sim = Simulator(eq,init = eq.inCond())
 #state = sim.states(4000,0.01)
 #sim.onedimplot(dim = [1])
->>>>>>> 1a7790caaec21a47f977dd4c7f8e8ad219db1797
