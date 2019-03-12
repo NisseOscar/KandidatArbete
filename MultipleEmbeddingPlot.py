150f1c510c77b37ad5f7ed611649d6c6343f9fee
CarolineBukk <caroline.bukk@gmail.com>
2019-03-12 16:58:00 +0100

    Updaterade den så att man bara behöver ändra på ett ställe om man ville ändra antalet dataset eller antalet grannar.

--- MultipleEmbeddingPlot.py
+++ MultipleEmbeddingPlot.py
@@ -21,18 +21,22 @@ sim = Simulator(eq)
 
 data = sim.states(duration=500)
 data = data[10000:]
-dataArr = [data[::10], data[::5]]
+dataArr = [data[::50], data[::40], data[::30], data[::20]]
 neighbors = range(6,16,2)
-n_datapoints = [len(dataArr[0][:,0]), len(dataArr[1][:,0])]
+n_datapoints = [len(dataArr[0][:,0]), len(dataArr[1][:,0]), len(dataArr[2][:,0]), len(dataArr[3][:,0])]
+nbr_dataSets = len(n_datapoints)
+print(str(nbr_dataSets))
+nbr_neighborSets = len(neighbors)
+print(str(nbr_neighborSets))
 
 fig = plt.figure()
 for i in range(10):
     #manifoldData = manifold.MDS(n_components=2, max_iter=100, n_init=1).fit_transform(data)
     #manifoldData, err = manifold.locally_linear_embedding(data, n_neighbors= neighbors[i%5], n_components=2,method = 'hessian') # weird results, can see that it is rossler but it doesn't look as excpected
-    manifoldData = manifold.Isomap(n_neighbors=5, n_components=2).fit_transform(dataArr[i%2])
-    ax = fig.add_subplot(5,2,i+1)
+    manifoldData = manifold.Isomap(n_neighbors=neighbors[i%nbr_neighborSets], n_components=2).fit_transform(dataArr[i%nbr_dataSets])
+    ax = fig.add_subplot(nbr_neighborSets,nbr_dataSets,i+1)
     ax.plot(manifoldData[:,0],manifoldData[:,1])
-    plt.title("k = " + str(neighbors[i%5]) + ", N = " + str(n_datapoints[i%2]))
+    plt.title("k = " + str(neighbors[i%nbr_neighborSets]) + ", N = " + str(n_datapoints[i%nbr_dataSets]))
 
 
 plt.show()
