a1d81369ec1302e0b306ca4810da275182a8dea0
adoJonsson <46928572+adoJonsson@users.noreply.github.com>
2019-03-27 12:09:25 +0100

    Added interpolateCurve to even out distances, still some index problems

--- Simulator.py
+++ Simulator.py
@@ -6,6 +6,8 @@ from scipy.integrate import solve_ivp
 from mpl_toolkits.mplot3d import Axes3D
 from scipy import signal
 import Equation
+from scipy.interpolate import splprep as spl
+from scipy.interpolate import splev
 
 class Simulator:
     def __init__(self,function,init = 0):
@@ -15,13 +17,14 @@ class Simulator:
     def states(self,duration=40,split=0.01):
         self.t = np.arange(0.0, duration, split)
         tmp = solve_ivp(self.function.f,[0, duration],self.state0,method='RK45',t_eval=self.t) # RK45, 4th order, 5th order error estimation, without t_eval we get large distances between the points
+        # tmp = solve_ivp(self.function.f,[0, duration],self.state0,method='RK45')
         self.data =  tmp.y
         #Return the transpose array of points
         return np.array(self.data).transpose()
 
     def tredimplot(self,dim = [0,1,2]):
         if self.data is None:
-            self.data()
+            self.states()
         states = self.data
         fig = plt.figure()
         ax = fig.gca(projection ='3d')
@@ -49,3 +52,40 @@ class Simulator:
         return self.t
     def getData(self):
         return self.data
+    def interpolateCurve(self):
+        if self.data is None:
+            self.states()
+        values = np.array(self.data)
+        np.swapaxes(values,1,0)
+        s = []
+        values2 =[]
+        lastPoint = values[:,0]
+        lastS = 0
+        for i, point in enumerate(values[0]):
+            point = values[:,i]
+            lastS = math.sqrt(np.dot(np.array(point-lastPoint),np.array(point-lastPoint)))+lastS
+            s.append(lastS)
+            values2.append(point)
+            lastPoint = point
+        values2 = np.array(values2)
+        s2 = np.linspace(s[0],s[-1],round(len(s)/2))
+        interpedData = []
+        for dim in [0,1,2]:
+            data = np.array(values2[:,dim]).reshape(len(s),1).squeeze()
+            dimData = []
+            s2Index = 0
+            lastIndex = 0
+            for i, value in enumerate(data):
+                if i % 10 == 0 and i+10 < len(s):
+                    tck, u = spl(np.array(data[i:i+9]).reshape(1,9),u = np.array(s[i:i+9]).reshape(9,1).flatten(),k = 5)
+                    while s2[s2Index] < s[i+9]:
+                        s2Index +=1
+                    if s2Index != lastIndex:
+                        newValues = splev(s2[lastIndex:s2Index],tck)
+                        for xi in newValues:
+                            for xij in xi:
+                                dimData.append(xij)
+                    lastIndex = s2Index
+            interpedData.append(dimData)
+        interpedData = np.array(interpedData)
+        return interpedData.transpose()
